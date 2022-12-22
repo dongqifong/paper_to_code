7ada9915be2b7464b27d291bf548b82babdb6393
qifong <qifong04@gmail.com>
2022-12-22 23:25:03 +0800

    update cbam

--- cbam/cbam.py
+++ cbam/cbam.py
@@ -11,17 +11,13 @@ import torch.nn as nn
 
 class ChannelAttention(nn.Module):
     def __init__(self, in_channels, ratio=None):
-        super(ChannelAttention, self).__init__()
-        
+        super().__init__()
         if ratio is None:
             ratio = 2
-            
         if ratio>in_channels:
             ratio = in_channels
-        
         self.avg_pool = nn.AdaptiveAvgPool1d(1)
         self.max_pool = nn.AdaptiveMaxPool1d(1)
-
         self.sharedMLP = nn.Sequential(
             nn.Conv1d(in_channels, in_channels // ratio, 1, bias=False), nn.ReLU(),
             nn.Conv1d(in_channels // ratio, in_channels, 1, bias=False))
@@ -30,24 +26,26 @@ class ChannelAttention(nn.Module):
     def forward(self, x):
         avgout = self.sharedMLP(self.avg_pool(x))
         maxout = self.sharedMLP(self.max_pool(x))
-        return self.sigmoid(avgout + maxout)
+        mask = self.sigmoid(avgout + maxout)
+        return mask
+    
     
 class SpatialAttention(nn.Module):
     def __init__(self, kernel_size=7):
-        super(SpatialAttention, self).__init__()
+        super().__init__()
         if kernel_size % 2 == 0:
             kernel_size = kernel_size -1
         padding = kernel_size // 2
-
         self.conv = nn.Conv1d(2,1,kernel_size, padding=padding, bias=False)
         self.sigmoid = nn.Sigmoid()
-
+        
     def forward(self, x):
         avgout = torch.mean(x, dim=1, keepdim=True)
         maxout, _ = torch.max(x, dim=1, keepdim=True)
         x = torch.cat([avgout, maxout], dim=1)
         x = self.conv(x)
-        return self.sigmoid(x)
+        mask = self.sigmoid(x)
+        return mask
     
     
 class CBAM(nn.Module):
@@ -67,8 +65,8 @@ class CBAM(nn.Module):
     
 if __name__ == "__main__":
     x = torch.randn((10,32,1000))
-    m = CBAM(in_channels=32,ratio=1.1,kernel_size=3)
-    mask = m(x)
-    print(mask.shape)
-    y = mask * x
+    m = CBAM(in_channels=32,ratio=1.1,kernel_size=1)
+    y = m(x)
     print(y.shape)
+    print(m.Mc.shape)  # torch.Size([10, 32, 1])
+    print(m.Ms.shape)  # torch.Size([10, 1, 1000])
